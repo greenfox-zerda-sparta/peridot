b345e6a2975873c337ff81eceacd4512c71193d5
Máté Németh <nmate91@gmail.com>
2017-02-02 19:25:22 +0100

    Email client fetches via IMAP

---
+++ EMail_Client_Beta/FirstColumnRefactor/MailFetcherFactory.cs
@@ -0,0 +1,26 @@
+﻿using EMail_Client_Beta.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMail_Client_Beta
+{
+    public static class MailFetcherFactory
+    {
+        public static IEmailFetcher CreateClient(MailboxConnectionInfo connectionInfo)
+        {
+            switch(connectionInfo.Protocol)
+            {
+                case Protocol.POP3:
+                case Protocol.POP3S:
+                    return new POP3MailFetcher(connectionInfo);
+                case Protocol.IMAP:
+                case Protocol.IMAPS:
+                default:
+                    return new IMAPMailFetcher(connectionInfo);
+            }
+        }
+    }
+}
